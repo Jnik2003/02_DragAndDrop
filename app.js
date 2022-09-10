973077f6dd8f7136fd7330864c6aba9ada37a54b
Georgii <kaunas16@gmail.com>
2022-09-10 08:34:39 +0300

    feat: Finish Project

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
 
-item.addEventListener('dragstart', dragstart)
-item.addEventListener('dragend', dragend)
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
 
 function dragstart(e){
 	e.target.classList.add('hold');
 	setTimeout(() => {
 		e.target.classList.add('hide');
-	},0)
-	
+	},0)	
 }
 
 function dragend(e){
 	e.target.classList.remove('hold', 'hide');
+}
+
+placeholders.forEach(item => {
+	item.addEventListener('dragover', dragover);
+	item.addEventListener('dragenter', dragenter);
+	item.addEventListener('dragleave', dragleave);
+	item.addEventListener('drop', dragdrop);
+})
+
+function dragover(e){
+	e.preventDefault();
+}
+function dragenter(e){
+	e.target.classList.add('hovered');
+}
+function dragleave(e){
+	e.target.classList.remove('hovered');
+}
+function dragdrop(e){
+	e.target.append(item);
+	e.target.classList.remove('hovered');
 }
